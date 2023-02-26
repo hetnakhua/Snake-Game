d10f2d3a10ef559af8660bbe5af5d1e0b1afcf1b
hetnakhua <hetsnakhua@gmail.com>
2023-02-26 05:36:33 +0530

    updated app name, description and icons

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Snake Game',
       home: HomePage(),
       theme: ThemeData(brightness: Brightness.dark),
     );
